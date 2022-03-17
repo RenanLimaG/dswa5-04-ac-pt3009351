7c205c186235a9c5a7e97203ca3c4b1246dc7884
RenanLimaG <renanlimag10@gmail.com>
2022-03-17 01:36:56 -0300

    Teste

--- public/js/controllers/ContatoController.js
+++ public/js/controllers/ContatoController.js
@@ -1,26 +1,4 @@
-angular.module('ifsp', ['ngRoute', 'ngResource']).config(function($routeProvider) {
-	
-	$routeProvider.when('/contatos', {
-		templateUrl: 'partials/contatos.html',
-		controller: 'ContatosController'
+angular.module('ifsp').controller('ContatoController',
+	function($scope, $routeParams) {
+		console.log($routeParams.contatoId);
 	});
-
-	$routeProvider.when('/contato/:contatoId', {
-    	templateUrl: 'partials/contato.html', 
-    	controller: 'ContatoController'
-    });
-
-    $routeProvider.when('/cursos', {
-		templateUrl: 'partials/cursos.html',
-		controller: 'CursosController'
-	});
-
-	$routeProvider.when('/curso/:contatoId', {
-    	templateUrl: 'partials/curso.html', 
-    	controller: 'CursoController'
-    });   
-
-
-    $routeProvider.otherwise({redirectTo: '/contatos'});
-
-});
